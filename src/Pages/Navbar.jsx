0bbfa05e21f1a475ff1f0375e939a6a691018bb2
SuryaBabu-1454 <suryababu1454@gmail.com>
2024-09-19 11:42:35 +0530

    cafe site successfully

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,41 +1,47 @@
-import React from 'react' 
-import '../index.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import '../index.css';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 const Navbar = () => {
   return (
-    <nav className="navbar sticky-top navbar-expand-lg navbar-dark  ">
-    <div className="container-fluid">
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <nav className="navbar sticky-top navbar-expand-lg navbar-dark">
+      <div className="container-fluid">
+      <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <Link className="navbar-brand" to="/"><b>Crew Spot</b></Link>
-        <ul className="navbar-nav  justify-content-center flex-grow-1 mb-2 pe-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="This-is-Us">This is us</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="menu">Menu</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="contact">Contact Us</Link>
-          </li>
-         
-        </ul>
-       
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <Link className="navbar-brand" to="/"><b>Crew Spot</b></Link>
+          <ul className="navbar-nav justify-content-center flex-grow-1 mb-2 pe-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="This-is-Us">This is us</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="menu">Menu</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="contact">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="login-page" className="nav-link">Login</Link>
       </div>
-    
-      <Link to="login-page" className="nav-link">Login</Link>
-      
+    </nav>
+  );
+};
 
-    </div>
-  </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
